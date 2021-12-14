aeceb42fc786d937acce816b3a8979c907af66cc
Samuel Walz <sw@noparking.dk>
2021-12-14 10:35:25 +0100

    search for first result

--- misc/ISearchProvider.cs
+++ misc/ISearchProvider.cs
@@ -12,5 +12,12 @@ namespace com.samwalz.unity_ui.misc
         /// <param name="maxResults">maximum number of search results, less or equal 0: unlimited</param>
         /// <returns>true, if at least 1 item found</returns>
         bool Search(string searchTerm, ref List<string> results, int maxResults = 0);
+        /// <summary>
+        /// Searches for first item that match a given search term
+        /// </summary>
+        /// <param name="searchTerm">term to search for</param>
+        /// <param name="result">found item</param>
+        /// <returns>true, if at least 1 item found</returns>
+        bool SearchFirst(string searchTerm, out string result);
     }
 }
